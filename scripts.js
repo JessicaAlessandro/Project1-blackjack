2363b1517e3e449a7013d4ee302f7dcabe9a87a7
Jessica Alessandro <JessicaAlessandro@Jessicas-MacBook-Pro.local>
2015-10-15 16:40:33 -0400

    added deal function

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
+var playerHand = [];
+var dealerHand = [];
+var cards = [];
+
 //create a card
 var card = function (value, name, suit){
 	this.value = value;
@@ -9,51 +13,83 @@ var card = function (value, name, suit){
 // create a function that will return an array of all 52 cards
 var deck = function (){
 	this.names = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
-	this.suits = ['Hearts','Diamonds','Spades','Clubs'];
-	var cards = [];
+	// set the suit to it's unicode character equivialant
+	this.suits = ['\u2665','\u2666','\u2660','\u2663'];
     
     for( s = 0; s < this.suits.length; s++ ) {
         for( n = 0; n < this.names.length; n++ ) {
+					// use an object constructor to create a new card 
             cards.push( new card( n+1, this.names[n], this.suits[s] ) );
         }
     }
+    // return cards;
+}
+// console.log(deck)
 
-    return cards;
+// RANDOMize the order of the cards math random
+function shuffle(deck) {
+  for (var i = deck.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * deck.length);
+      var temp = deck[i];
+      deck[i] = deck[j];
+      deck[j] = temp;
+  }
+  return deck;
 }
 
-var newDeck = new deck();
-
-// console.log(newDeck);
 
 
-window.onload= function (e){
-	//loop through each card
-	for(var i=0; i < newDeck.length; i++){
-		//create div for cards
-		var $div = $('<div></div>')
+
+
+
+
+
+
+// create a function that will deal and display the cards from the deck $('#deal-button');
+$(function () {
+
+	// grab DOM elements
+	var $dealButton = $('#deal-button');
+
+	// start game event
+	deck();
+	shuffle(cards);
+	console.log(cards.length);
+	
+
+	// click events
+	$dealButton.click(function(){
+	 		console.log('clicked');
+	 		var newCard1 = cards.pop();
+	 		var newCard2 = cards.pop();
+	 		// put card into playerhand or dealerhand
+	 		// somehow . . . 
+	 		console.log(newCard);
+	 		console.log(cards.length);
+	 		// displayPlayerCards();
+	 		// displayDealerCards
+	 		// checkWin();
+	 });
+
+	var displayPlayerCards = function() {
+		// clear the div that contains player cards
+		// render everything within the dealerhand array
+		newCard1
+		newCard2
+
+		$playerHand = $(".player-cards")
+		$playerCards = $('<div></div>')
+		$playerCards.addClass('card');
+		$playerHand.append(playerCards)
 		$div.addClass('card');
-			// console.log($div)
 
-		//set the suit to it's unicode character equivialant, and add css element to add color red to hearts and diamonds
-		if(newDeck[i].suit == 'Hearts'){
-			$div.css('color', 'red')
-			var character = "\u2665";
-		} else if (newDeck[i].suit == 'Diamonds'){
-			$div.css('color', 'red')
-			var character = "\u2666";
-		}else if (newDeck[i].suit == 'Spades'){
-			var character = "\u2660";
-		} else{
-			var character = "\u2663"
-		}
-
-		//add text to new div that will have the name and the suit on unicode
 		$div.html('' + newDeck[i].name + '<br>' + character + '');
 		$('#container').append($div)
+
+
+
 	}
-}
+	
+	var displayDealerCards = function(){}
 
-// RANDOMize the order of the cards usinf sort and math random
-
-newDeck.sort(function() { return 0.5 - Math.random() }); 
-
+});
